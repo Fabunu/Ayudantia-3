f92d83167d422b2aa40cfddd622b0c271420eed0
Fabunu <f.cheuquelaf01@ufromail.cl>
2024-09-16 12:28:41 -0300

    funcional

--- evidencia/src/main/java/ayudantia/JuegoMatriz.java
+++ evidencia/src/main/java/ayudantia/JuegoMatriz.java
@@ -1,60 +1,61 @@
-package  ayudantia;
+package ayudantia;
 import java.util.Random;
 import java.util.Scanner;
 
 public class JuegoMatriz {
     
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); //Lee la entrada del usuario
-        //Inicializar el mapa y los atributos del personaje
+        Scanner scanner = new Scanner(System.in);
         String[][] mapa = inicializarMapa();
-        int[] posicionPersonaje = {0, 0};  // Coordenadas iniciales
+
+        int[] posicionPersonaje = {1, 1};
         int vidaPersonaje = 100;
         int ataquePersonaje = 15;
-        int[][] enemigos = {{2, 2, 45, 10}};  // Coordenadas y atributos del enemigo {x, y, vida, ataque}
+        int[][] enemigos = {{2, 2, 45, 10}}; // Enemigo en (2, 2)
 
         iniciarJuego(mapa, posicionPersonaje, vidaPersonaje, ataquePersonaje, enemigos, scanner);
     }
 
     public static String[][] inicializarMapa() {
         String[][] mapa = new String[10][10];
-        //Rellenar el mapa con puntos
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 mapa[i][j] = ".";
             }
         }
-        mapa[0][0] = "#";
-        mapa[0][9] = "#";
-        mapa[9][0] = "#";
-        mapa[9][9] = "#";
 
-        mapa[1][0] = "#"; // Colocar obstáculos adicionales
-        mapa[0][1] = "#";
-        mapa[1][1] = "#";
-        mapa[8][9] = "#";
-        mapa[9][8] = "#";
-        mapa[8][8] = "#";
+        for (int i = 0; i < 10; i++) {
+            mapa[0][i] = "#";
+            mapa[9][i] = "#";
+            mapa[i][0] = "#";
+            mapa[i][9] = "#";
+        }
+        
+        colocarObstaculos(mapa);
+        mapa[8][8] = "X"; // Destino
+        mapa[2][2] = "E"; // Enemigo
+        mapa[5][5] = "C"; // Cofre
 
-        mapa[0][0] = "P"; //Posicion inicial del personaje
-        mapa[9][9] = "X"; //Destino
-        mapa[3][3] = "#"; //Obstaculo
-        mapa[2][2] = "E"; //Enemigo
-        mapa[5][5] = "C"; //Cofre
         return mapa;
+    }
 
+    public static void colocarObstaculos(String[][] mapa) {
+        mapa[1][3] = "#";
+        mapa[1][1] = "#";
+        mapa[5][4] = "#";
+        mapa[2][1] = "#";
+        mapa[8][1] = "#";
     }
 
     public static void iniciarJuego(String[][] mapa, int[] posicionPersonaje, int vidaPersonaje, int ataquePersonaje, int[][] enemigos, Scanner scanner) {
         boolean juegoActivo = true;
-        boolean enemigoDerrotado = false;
-        //buble principal del juego
-        while(juegoActivo) {
+
+        while (juegoActivo) {
             imprimirMapa(mapa, posicionPersonaje);
             System.out.println("Mover: w(arriba), a(izquierda), d(derecha), s(abajo)");
             String comando = scanner.nextLine();
 
-            if (comando.equalsIgnoreCase("Salir")) {
+            if (comando.equalsIgnoreCase("salir")) {
                 System.out.println("Juego terminado...");
                 break;
             }
@@ -62,11 +63,11 @@ public class JuegoMatriz {
             int[] nuevaPosicion = moverPersonaje(mapa, posicionPersonaje, comando);
             if (nuevaPosicion != null) {
                 posicionPersonaje = nuevaPosicion;
-                vidaPersonaje = verificarEvento(mapa, posicionPersonaje, vidaPersonaje, ataquePersonaje, enemigos, scanner)
+                vidaPersonaje = verificarEvento(mapa, posicionPersonaje, vidaPersonaje, ataquePersonaje, enemigos, scanner);
             }
-            
+
             if (vidaPersonaje <= 0) {
-                System.out.println("Has perdido toda tu vida. Fin del juego :()");
+                System.out.println("Has perdido toda tu vida. Fin del juego :(");
                 break;
             }
         }
@@ -96,40 +97,36 @@ public class JuegoMatriz {
             case "a": nuevaY--; break;
             case "d": nuevaY++; break;
             default:
-                System.out.println("Comando no valido");
+                System.out.println("Comando no válido");
                 return null;
         }
-        //Ve si la nueva posicion es valida
+
         if (esPosicionValida(mapa, nuevaX, nuevaY)) {
             mapa[posicionPersonaje[0]][posicionPersonaje[1]] = ".";
-            posicionPersonaje[0] = nuevaX;
-            posicionPersonaje[1] = nuevaY;
-            mapa[nuevaX][nuevaY] = "P";
-            return posicionPersonaje;
+            return new int[] {nuevaX, nuevaY};
         } else {
-            System.out.println("No puedes moverte a esa posicion");
-            return  null;
+            System.out.println("No puedes moverte a esa posición");
+            return null;
         }
     }
 
     public static boolean esPosicionValida(String[][] mapa, int x, int y) {
-        if (x < 0 || x >= 10 || y < 0 || y >= 10 || mapa[x][y].equals("#")) {
-            return false;
-        }
-        return true;
+        return !(x < 0 || x >= 10 || y < 0 || y >= 10 || mapa[x][y].equals("#"));
     }
 
-    public static int verificarEvento(String[][] mapa, int[] posicionPersonaje, int vidaPersonaje, int ataquePersonaje, int[][] enemigos, boolean enemigoDerrotado, Scanner scanner) {
+    public static int verificarEvento(String[][] mapa, int[] posicionPersonaje, int vidaPersonaje, int ataquePersonaje, int[][] enemigos, Scanner scanner) {
         String evento = mapa[posicionPersonaje[0]][posicionPersonaje[1]];
 
         if (evento.equals("E")) {
-            System.out.println("Oh, no, enemigo encontrado... Inicia el combate");
+            System.out.println("Oh no, enemigo encontrado... Inicia el combate");
             vidaPersonaje = iniciarCombate(vidaPersonaje, ataquePersonaje, enemigos, scanner);
-            mapa[posicionPersonaje[0]][posicionPersonaje[1]] = ".";
+            if (vidaPersonaje > 0) {
+                mapa[posicionPersonaje[0]][posicionPersonaje[1]] = "."; // Eliminar enemigo
+            }
         } else if (evento.equals("C")) {
             System.out.println("Has encontrado un cofre");
             vidaPersonaje = abrirCofre(vidaPersonaje);
-            mapa[posicionPersonaje[0]][posicionPersonaje[1]] = ".";
+            mapa[posicionPersonaje[0]][posicionPersonaje[1]] = "."; // Eliminar cofre
         } else if (evento.equals("X")) {
             System.out.println("Has llegado a tu destino, ganaste :]");
             System.exit(0);
@@ -151,14 +148,14 @@ public class JuegoMatriz {
 
             System.out.println("El enemigo ataca");
             vidaPersonaje -= ataqueEnemigo;
-            if(vidaPersonaje <= 0) {
+            if (vidaPersonaje <= 0) {
                 System.out.println("Has perdido toda tu vida");
                 return vidaPersonaje;
             }
 
-            System.out.println("Quieres intentar huir? (si/no)");
+            System.out.println("¿Quieres intentar huir? (si/no)");
             String decision = scanner.nextLine();
-            if (decision.equals("si")) {
+            if (decision.equalsIgnoreCase("si")) {
                 System.out.println("Has huido del combate");
                 return vidaPersonaje;
             }
@@ -177,5 +174,4 @@ public class JuegoMatriz {
         }
         return vidaPersonaje;
     }
-
 }
